7fdf49f0c770fa39026d2b89b4ebac20015366ec
Maksym Operchuk <operlast529@gmail.com>
2024-05-11 16:01:52 +0300

    Create: get_exchange_rate function xpath_parser.py file.

---
+++ parser/xpath_parser.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from lxml import html
+
+
+load_dotenv()
+
+URL = os.getenv("RATE_URL")
+RATE_SEARCH_PATTERN = "//div[@jsname='LXPcOd']//div[@jsname='ip75Cb']/div/text()"
+
+
+def get_exchange_rate() -> str:
+    response = requests.get(URL)
+    html_content = response.text
+    tree = html.fromstring(html_content)
+
+    rate = tree.xpath(RATE_SEARCH_PATTERN)
+
+    return rate[0]
