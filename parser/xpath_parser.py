c8bfec3208883bf46c1ee15cec6822e922053a67
Maksym Operchuk <operlast529@gmail.com>
2024-05-11 16:56:59 +0300

    Update: return annotation in get_exchange_rate function.

--- parser/xpath_parser.py
+++ parser/xpath_parser.py
@@ -11,11 +11,11 @@ URL = os.getenv("RATE_URL")
 RATE_SEARCH_PATTERN = "//div[@jsname='LXPcOd']//div[@jsname='ip75Cb']/div/text()"
 
 
-def get_exchange_rate() -> str:
+def get_exchange_rate() -> float:
     response = requests.get(URL)
     html_content = response.text
     tree = html.fromstring(html_content)
 
     rate = tree.xpath(RATE_SEARCH_PATTERN)
 
-    return rate[0]
+    return float(rate[0])
