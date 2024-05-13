75fb3488d68e941ebceb815a50e624e6bfe14bce
Maksym Operchuk <operlast529@gmail.com>
2024-05-13 19:58:52 +0300

    Update: create_table function in create_db.py file.

--- database/create_db.py
+++ database/create_db.py
@@ -1,10 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
 from database.connect_db import get_database_connection
+
+
+load_dotenv()
+
+TABLE_NAME = os.getenv("TABLE_NAME")
 
 
 def create_table() -> None:
     authors_table_creation_query = (
-        """CREATE TABLE IF NOT EXISTS 
-        exchange_rates(date TEXT, usd_to_uah FLOAT)"""
+        f"""
+        CREATE TABLE IF NOT EXISTS 
+        {TABLE_NAME}(date TEXT, usd_to_uah FLOAT)
+        """
     )
 
     con = get_database_connection()
