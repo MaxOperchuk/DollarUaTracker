66202735d9a9a900c605a7a5d7b678f3a0384389
Maksym Operchuk <operlast529@gmail.com>
2024-05-14 17:22:17 +0300

    Add: time_zone fixes to tasks.py file.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-
+import pytz
 from celery import Celery
 from celery.schedules import crontab
 from dotenv import load_dotenv
@@ -12,15 +12,16 @@ from parser.xpath_parser import get_exchange_rate
 load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME")
+TIME_ZONE = os.getenv("TIME_ZONE")
 
-app = Celery("tasks", broker="redis://localhost:6379")
+app = Celery("tasks", broker="redis://redis:6379")
 app.conf.broker_connection_retry_on_startup = True
 
-
+app.conf.timezone = TIME_ZONE
 app.conf.beat_schedule = {
     "update-exchange-rate": {
         "task": "tasks.parse_exchange_rate",
-        "schedule": crontab(hour="*"),
+        "schedule": crontab(minute="0", hour="*"),
     },
 }
 
@@ -28,7 +29,10 @@ app.conf.beat_schedule = {
 @app.task(bind=True)
 def parse_exchange_rate(self):
     exchange_rate = get_exchange_rate()
-    current_time = datetime.now()
+
+    tz = pytz.timezone(TIME_ZONE)
+    current_time = datetime.now(tz=tz)
+
     populate_table(time=current_time, exchange_rate=exchange_rate)
 
     return f"Exchange rate {exchange_rate} was saved at {current_time}."
