5a6f2d7f4b23a54dcc76cfb935ce1ab3e248fed7
Maksym Operchuk <operlast529@gmail.com>
2024-05-14 17:20:54 +0300

    Add: FILE_NAME const to xlsx_writer.py file.

--- writer/xlsx_writer.py
+++ writer/xlsx_writer.py
@@ -1,11 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from openpyxl import Workbook
 
 
-def write_to_xlsx(data):
+load_dotenv()
+
+FILE_NAME = os.getenv("FILE_NAME")
+
+
+def write_to_xlsx(data: list) -> None:
     wb = Workbook()
     ws = wb.active
 
     for row in data:
         ws.append(row)
 
-    wb.save("exchange_rates.xlsx")
+    wb.save(FILE_NAME)
