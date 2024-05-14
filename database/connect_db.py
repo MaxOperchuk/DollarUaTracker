f4968d3bcedf1450b7e24c2159865492cb91e352
Maksym Operchuk <operlast529@gmail.com>
2024-05-14 17:26:36 +0300

    Light-fixes: connect_db.py file.

--- database/connect_db.py
+++ database/connect_db.py
@@ -7,11 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME")
-ABS_PATH_TO_PROJECT_DIR = os.getenv("ABS_PATH_TO_PROJECT_DIR")
 
 
 def get_database_connection() -> sqlite3.Connection:
-    os.chdir(ABS_PATH_TO_PROJECT_DIR)
-    con = sqlite3.connect(os.path.abspath(DB_NAME))
+    con = sqlite3.connect(DB_NAME)
 
     return con
