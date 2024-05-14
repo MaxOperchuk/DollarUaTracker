16f0d5bc9e559c0b4382aff472b7f3c9e9b561dc
Maksym Operchuk <operlast529@gmail.com>
2024-05-14 17:25:43 +0300

    Add: time-zone configuration to extract_data.py file.

--- database/extract_data.py
+++ database/extract_data.py
@@ -1,6 +1,13 @@
 import os
+import sys
+from datetime import datetime
 
+import pytz
 from dotenv import load_dotenv
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
 
 from database.connect_db import get_database_connection
 
@@ -8,12 +15,20 @@ from database.connect_db import get_database_connection
 load_dotenv()
 
 TABLE_NAME = os.getenv("TABLE_NAME")
+TIME_ZONE = os.getenv("TIME_ZONE")
 
 
 def fetch_data():
     conn = get_database_connection()
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {TABLE_NAME}")
+
+    tz = pytz.timezone(TIME_ZONE)
+    current_date = datetime.now(tz=tz).date()
+
+    cursor.execute(
+        f"SELECT * FROM {TABLE_NAME} "
+        f"WHERE date LIKE '{current_date}%'"
+    )
 
     rows = cursor.fetchall()
     conn.close()
