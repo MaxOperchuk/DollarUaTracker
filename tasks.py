7728d4e86497f4c589561469382e9fcaea750e79
Maksym Operchuk <operlast529@gmail.com>
2024-05-15 12:43:02 +0300

    Light-fixes: remove celery config from tasks.py and fix imports.

--- tasks.py
+++ tasks.py
@@ -1,29 +1,18 @@
 import os
 from datetime import datetime
+
 import pytz
-from celery import Celery
-from celery.schedules import crontab
 from dotenv import load_dotenv
 
-from database.load_data_to_db import populate_table
-from parser.xpath_parser import get_exchange_rate
+from celery_conf import app
+from src.database.load_data_to_db import populate_table
+from src.parser.xpath_parser import get_exchange_rate
 
 
 load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME")
 TIME_ZONE = os.getenv("TIME_ZONE")
-
-app = Celery("tasks", broker="redis://redis:6379")
-app.conf.broker_connection_retry_on_startup = True
-
-app.conf.timezone = TIME_ZONE
-app.conf.beat_schedule = {
-    "update-exchange-rate": {
-        "task": "tasks.parse_exchange_rate",
-        "schedule": crontab(minute="0", hour="*"),
-    },
-}
 
 
 @app.task(bind=True)
